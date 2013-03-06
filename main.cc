e6e62aaa39686e5504d76fb7095f7c4ac6468b61
Morten Hustveit <morten.hustveit@gmail.com>
2013-03-05 16:22:57 -0800

    Support the --color and --cover-threshold options
    --color treats every other byte as a color for the following byte, allowing
    characters to be treated differently depending on context.  For example, you
    may want to treat HTML tags differently from other text within HTML documents.
    
    --cover-threshold sets a lower bound for the incremental covered required to
    accept a new substring into the covering set.
    
    Signed-off-by: Morten Hustveit <morten.hustveit@gmail.com>

--- main.cc
+++ main.cc
@@ -26,31 +26,33 @@ static int skip_samecount_prefixes;
 static int do_probability;
 static int do_unique;
 static int do_document;
+static int do_color;
 static int do_cover;
 static double prior_bias = 1.0;
 static double threshold;
+static int cover_threshold;
 
 static struct option long_options[] =
 {
-  { "cover",             no_argument,       &do_cover,                1 },
-  { "documents",         no_argument,       &do_document,             1 },
+  { "color",             no_argument,       &do_color,                  1 },
+  { "cover",             no_argument,       &do_cover,                  1 },
+  { "cover-threshold",   required_argument, NULL,                     'c' },
+  { "documents",         no_argument,       &do_document,               1 },
   { "prior-bias",        required_argument, NULL,                     'p' },
-  { "probability",       no_argument,       &do_probability,          1 },
-  { "skip-prefixes",     no_argument,       &skip_samecount_prefixes, 1 },
+  { "probability",       no_argument,       &do_probability,            1 },
+  { "skip-prefixes",     no_argument,       &skip_samecount_prefixes,   1 },
   { "threshold",         required_argument, NULL,                     't' },
-  { "unique-substrings", no_argument,       &do_unique,               1 },
-  { "version",           no_argument,       &print_version,           1 },
-  { "help",              no_argument,       &print_help,              1 },
+  { "unique-substrings", no_argument,       &do_unique,                 1 },
+  { "version",           no_argument,       &print_version,             1 },
+  { "help",              no_argument,       &print_help,                1 },
   { 0, 0, 0, 0 }
 };
 
-static const char *input0;
-static saidx_t *input0_suffixes;
-static size_t input0_size;
+static const char *input0, *input1;
+static size_t input0_size, input1_size;
 
-static const char *input1;
-static saidx_t *input1_suffixes;
-static size_t input1_size;
+static saidx_t *input0_suffixes, *input1_suffixes;
+size_t input0_suffix_count, input1_suffix_count;
 
 static std::vector<size_t> input0_n_gram_counts;
 static std::vector<size_t> input1_n_gram_counts;
@@ -117,12 +119,20 @@ struct substring
   size_t count;
 };
 
-template<typename I>
 static void
-print_string (I ch, size_t length)
+print_string (const char *string, size_t length)
 {
+  const unsigned char *ch = (const unsigned char *) string;
+
   for (; length--; ++ch)
     {
+      if (do_color && length)
+        {
+          printf ("\033[%d;1m", *ch - 'A' + 30);
+          ++ch;
+          --length;
+        }
+
       if (isprint (*ch) || (*ch & 0x80))
         {
           putchar (*ch);
@@ -145,6 +155,9 @@ print_string (I ch, size_t length)
         default: printf ("%03o", (unsigned char) *ch);
         }
     }
+
+  if (do_color)
+    printf ("\033[00m");
 }
 
 struct compare_in_input1
@@ -201,52 +214,46 @@ find_substrings (size_t input0_threshold, size_t input1_threshold)
   const char *input0_end;
   size_t previous_prefix = 0;
 
-  if (!input0_size)
+  if (!input0_suffix_count)
     return;
 
   input0_end = input0 + input0_size;
 
   std::vector<size_t> shared_prefixes;
 
-  shared_prefixes.reserve (input0_size);
+  shared_prefixes.reserve (input0_suffix_count);
 
-  for (size_t i = 0; i + 1 < input0_size; ++i)
+  for (size_t i = 0; i + 1 < input0_suffix_count; ++i)
     {
       const char *lhs, *rhs;
+      size_t shared_prefix;
 
       lhs = input0 + input0_suffixes[i];
       rhs = input0 + input0_suffixes[i + 1];
 
-      shared_prefixes.push_back (common_prefix (lhs, rhs,
-                                                std::min ((size_t) (input0_end - lhs),
-                                                          (size_t) (input0_end - rhs))));
+      shared_prefix = common_prefix (lhs, rhs,
+                                     std::min ((size_t) (input0_end - lhs),
+                                               (size_t) (input0_end - rhs)));
+
+      if (do_color)
+        shared_prefix &= ~1;
+
+      shared_prefixes.push_back (shared_prefix);
     }
 
   std::vector<substring> stack;
   std::set<size_t> matching_documents;
 
-  for (size_t i = 1; i < input0_size; ++i)
+  for (size_t i = 1; i < input0_suffix_count; ++i)
     {
-      const char *current;
       size_t prefix;
-
-      matching_documents.clear ();
-
-      current = input0 + input0_suffixes[i];
-
-      /* XXX: Use the shared_prefixes array to skip faster */
-
-      if (current[0] == DELIMITER)
-        continue;
-
-      /* UTF-8 continuation byte */
-      if ((current[0] & 0xc0) == 0x80)
-        continue;
 
       prefix = shared_prefixes[i - 1];
 
       if (prefix > previous_prefix)
         {
+          matching_documents.clear ();
+
           size_t count = 2;
           size_t j = i + 1;
 
@@ -261,7 +268,7 @@ find_substrings (size_t input0_threshold, size_t input1_threshold)
                             input0_suffixes[i]);
             }
 
-          for (size_t length = prefix; j <= input0_size && length > previous_prefix; )
+          for (size_t length = prefix; j <= input0_suffix_count && length > previous_prefix; )
             {
               if (shared_prefixes[j - 1] < length)
                 {
@@ -276,7 +283,10 @@ find_substrings (size_t input0_threshold, size_t input1_threshold)
                       stack.push_back (s);
                     }
 
-                  --length;
+                  if (do_color)
+                    length -= 2;
+                  else
+                    --length;
 
                   continue;
                 }
@@ -305,31 +315,33 @@ find_substrings (size_t input0_threshold, size_t input1_threshold)
               if (s.count < input0_threshold)
                 continue;
 
+              fprintf (stderr, "Hello\n");
+
               saidx_t *end, *search_result;
 
               do
                 {
-                  end = input1_suffixes + std::min (input1_offset + 1024, input1_size);
+                  end = input1_suffixes + std::min (input1_offset + 1024, input1_suffix_count);
 
                   search_result = std::lower_bound (input1_suffixes + input1_offset, end,
                                                     s.text, compare_in_input1 (s.length));
 
                   input1_offset = search_result - input1_suffixes;
                 }
-              while (search_result == end && end != input1_suffixes + input1_size);
+              while (search_result == end && end != input1_suffixes + input1_suffix_count);
 
               input1_match_end = input1_offset;
 
               do
                 {
-                  end = input1_suffixes + std::min (input1_match_end + 1024, input1_size);
+                  end = input1_suffixes + std::min (input1_match_end + 1024, input1_suffix_count);
 
                   search_result = std::upper_bound (input1_suffixes + input1_match_end, end,
                                                     s.text, compare_in_input1 (s.length));
 
                   input1_match_end = search_result - input1_suffixes;
                 }
-              while (search_result == end && end != input1_suffixes + input1_size);
+              while (search_result == end && end != input1_suffixes + input1_suffix_count);
 
               if (do_document)
                 {
@@ -472,7 +484,7 @@ find_cover (void)
       size_t string_length = j->string_length;
 
       std::list<std::pair<const char *, size_t> >::iterator k;
-      size_t hits = 0;
+      int hits = 0;
 
       for (k = remaining_documents.begin ();
            k != remaining_documents.end (); )
@@ -488,9 +500,9 @@ find_cover (void)
             ++k;
         }
 
-      if (hits)
+      if (hits > cover_threshold)
         {
-          printf ("%zu\t", hits);
+          printf ("%d\t", hits);
           print_string (string_begin, string_length);
           putchar ('\n');
         }
@@ -610,6 +622,37 @@ find_document_bounds (std::vector<size_t>& document_ends,
   assert (!document_ends.empty ());
 }
 
+static size_t
+filter_suffixes (saidx_t *input, const char *text, size_t count)
+{
+  saidx_t *output, *i, *end;
+
+  i = input;
+  end = i + count;
+
+  output = input;
+
+  for (; i != end; ++i)
+    {
+      int ch;
+
+      if (do_color && (*i & 1))
+        continue;
+
+      ch = (unsigned char) text[*i];
+
+      if (ch == DELIMITER)
+        continue;
+
+      if ((ch & 0xc0) == 0x80)
+        continue;
+
+      *output++ = *i;
+    }
+
+  return output - input;
+}
+
 int
 main (int argc, char **argv)
 {
@@ -622,6 +665,15 @@ main (int argc, char **argv)
       switch (i)
         {
         case 0:
+
+          break;
+
+        case 'c':
+
+          cover_threshold = strtol (optarg, &endptr, 0);
+
+          if (*endptr || cover_threshold < 0)
+            errx (EX_USAGE, "Parse error in cover threshold, expected non-negative integer");
 
           break;
 
@@ -735,8 +787,10 @@ main (int argc, char **argv)
     }
 
   divsufsort ((const sauchar_t *) input0, input0_suffixes, input0_size);
-
   divsufsort ((const sauchar_t *) input1, input1_suffixes, input1_size);
+
+  input0_suffix_count = filter_suffixes (input0_suffixes, input0, input0_size);
+  input1_suffix_count = filter_suffixes (input1_suffixes, input1, input1_size);
 
   if (do_unique)
     skip_samecount_prefixes = 1;
